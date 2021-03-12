72b5bccf32d587b743a36bf4fd7bff33a5697485
amaliajimenezajt <amaliajimenez.ajt@gmail.com>
2021-03-12 12:40:37 +0100

    error upload the data

--- app.R
+++ app.R
@@ -7,4 +7,7 @@ devtools::install_git("https://github.com/bernardo-dauria/kaggler.git")
 library(kaggler)
 kaggler::kgl_auth(username="amaliajimenezt",key="8e177825f7c983276080202675cc94f6")
 
-kgl_competitions_list_
+
+bank <- kgl_datasets_download(owner_dataset = "amaliajimenezt/bank-churners", 
+fileName = "bank_data_churners.csv")
+
